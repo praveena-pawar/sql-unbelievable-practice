de63d9f9b732fff05ecdd8272067955cb39dc59a
praveena-pawar <praveena.dev02@gmail.com>
2025-12-16 20:42:05 +0530

    inserting data for products and orders

--- insert_data.sql
+++ insert_data.sql
@@ -5,3 +5,22 @@ INSERT INTO customers VALUES
 (3, 'Rahul', 'Delhi', '2024-03-20'),
 (4, 'Priya', 'Chennai', '2024-04-05'),
 (5, 'Karan', 'Hyderabad', '2024-05-12');
+
+
+INSERT INTO products VALUES
+(101, 'Laptop', 'Electronics', 60000),
+(102, 'Mobile', 'Electronics', 30000),
+(103, 'Headphones', 'Accessories', 2000),
+(104, 'Keyboard', 'Accessories', 1500),
+(105, 'Chair', 'Furniture', 5000);
+
+
+INSERT INTO orders VALUES
+(1001, 1, '2024-06-01', 'Completed'),
+(1002, 2, '2024-06-03', 'Completed'),
+(1003, 3, '2024-06-05', 'Pending'),
+(1004, 1, '2024-06-10', 'Completed'),
+(1005, 4, '2024-06-12', 'Completed'),
+(1006, 5, '2024-06-15', 'Cancelled'),
+(1007, 2, '2024-06-18', 'Completed'),
+(1008, 3, '2024-06-20', 'Completed');
