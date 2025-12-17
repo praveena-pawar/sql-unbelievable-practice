9411c377880a1b2ec12a80474211c1a73be097f6
praveena-pawar <praveena.dev02@gmail.com>
2025-12-17 18:12:11 +0530

    Find the total number of orders placed by each customer.

--- level-1-basics.sql
+++ level-1-basics.sql
@@ -6,3 +6,14 @@ FROM customers c
 LEFT JOIN orders o
     ON c.customer_id = o.customer_id
 WHERE o.customer_id IS NULL;
+
+
+
+
+-- - LEVEL 1 – QUESTION 2 (next basic)
+-- Find the total number of orders placed by each customer.
+SELECT c.customer_name, c.customer_id, COUNT(o. order_id) AS total_number_of_orders
+FROM customers c
+LEFT JOIN orders o
+    ON c.customer_id = o.customer_id
+GROUP BY c.customer_name, c.customer_id;
