7e426f1825517b5870efc39646b5b679e446bcca
praveena-pawar <praveena.dev02@gmail.com>
2026-03-10 18:03:06 +0530

    dataset for subquery

---
+++ subqueries/dataset.sql
@@ -0,0 +1,24 @@
+CREATE TABLE employees (
+    id INT,
+    name VARCHAR(50),
+    department VARCHAR(50),
+    salary INT,
+    age INT
+);
+
+INSERT INTO employees VALUES
+(1,'Alice','Engineering',90000,28),
+(2,'Bob','Engineering',85000,30),
+(3,'Charlie','Engineering',90000,26),
+(4,'David','HR',60000,35),
+(5,'Eva','HR',65000,29),
+(6,'Frank','HR',55000,40),
+(7,'Grace','Sales',70000,31),
+(8,'Hannah','Sales',72000,27),
+(9,'Ian','Sales',68000,33),
+(10,'Jack','Marketing',75000,29),
+(11,'Kelly','Marketing',78000,32),
+(12,'Leo','Marketing',72000,26),
+(13,'Mona','Engineering',95000,34),
+(14,'Nina','Engineering',88000,25),
+(15,'Oscar','Sales',69000,36);
