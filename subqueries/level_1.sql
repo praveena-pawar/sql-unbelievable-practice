5261da13686a6ef1c952cc2b3c4ffdac8b24932c
praveena-pawar <praveena.dev02@gmail.com>
2026-03-10 19:52:13 +0530

    solved query 1

--- subqueries/level_1.sql
+++ subqueries/level_1.sql
@@ -1,2 +1,10 @@
 -- problem 1 :
 -- Find the names of employees whose salary is greater than the average salary of their department.
+
+SELECT name, department, salary
+FROM employees e1
+WHERE salary > (
+    SELECT AVG(salary)
+    FROM employees e2
+    WHERE e1.department = e2.department
+);
