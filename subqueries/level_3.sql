f5c2f46c84564254f0b1ae345ceaef4c3f266bf7
praveena-pawar <praveena.dev02@gmail.com>
2026-03-16 17:55:10 +0530

    Query 2 : Find the employees who have the highest salary in their department.

--- subqueries/level_3.sql
+++ subqueries/level_3.sql
@@ -6,3 +6,8 @@ WHERE salary > (SELECT AVG(salary)
 	FROM employees e2
     WHERE e1.department = e2.department
 );
+
+
+
+-- Query 2:
+-- Find the employees who have the highest salary in their department.
