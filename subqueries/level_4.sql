8336127029cf3da9f744c42c9bf6a3992b0ef425
praveena-pawar <praveena.dev02@gmail.com>
2026-03-17 22:39:13 +0530

    solved query 1

--- subqueries/level_4.sql
+++ subqueries/level_4.sql
@@ -1,2 +1,8 @@
 -- Query 1 :
 -- Find the employees who work in departments that have at least one employee earning more than 90000.
+SELECT name, department, salary
+FROM employees
+WHERE department IN (SELECT department
+	FROM employees 
+    WHERE salary > 90000
+);
