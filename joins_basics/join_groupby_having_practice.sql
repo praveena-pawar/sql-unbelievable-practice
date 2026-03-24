1663cc3598584e11e72ab1453fb47f51821f3803
praveena-pawar <praveena.dev02@gmail.com>
2026-03-24 20:40:35 +0530

    solved Query 2

--- joins_basics/join_groupby_having_practice.sql
+++ joins_basics/join_groupby_having_practice.sql
@@ -15,3 +15,10 @@ HAVING AVG(o.amount) > 400;
 
 -- Query2 : List all customers who have made more than one order, along with the number of orders they have made. 
 -- Sort the results by the number of orders in descending order.
+SELECT name,
+       COUNT(o.order_id) AS number_of_orders
+FROM customers c
+INNER JOIN orders o ON c.customer_id = o.customer_id
+GROUP BY name
+HAVING COUNT(o.order_id) > 1
+ORDER BY COUNT(o.order_id) DESC ;
