5c184f769577b25208109749c48a8d33e0436882
praveena-pawar <praveena.dev02@gmail.com>
2026-03-24 20:51:31 +0530

    solved Query1

--- joins_basics/join_subquery_practice.sql
+++ joins_basics/join_subquery_practice.sql
@@ -1,2 +1,11 @@
 -- Query1 : Get employee name, department name, salary
 -- Only include employees whose salary is greater than the average salary of all employees
+
+SELECT e.name,
+       d.dept_name,
+       e.salary
+FROM employee e
+INNER JOIN department d ON e.dept_id = d.dept_id
+WHERE e.salary >
+    (SELECT AVG(salary)
+     FROM employee);
