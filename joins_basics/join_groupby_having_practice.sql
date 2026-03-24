b48e69be2f33ff82ec68fbb7b2d8de783a0ef392
praveena-pawar <praveena.dev02@gmail.com>
2026-03-24 20:25:27 +0530

    solved query 1

--- joins_basics/join_groupby_having_practice.sql
+++ joins_basics/join_groupby_having_practice.sql
@@ -3,3 +3,9 @@
 -- Condition: total > 800
 -- only customers who placed orders
 
+SELECT name,
+       SUM(o.amount) AS total_order_amount
+FROM customers c
+INNER JOIN orders o ON c.customer_id = o.customer_id
+GROUP BY name
+HAVING AVG(o.amount) > 400;
