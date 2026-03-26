5165188a7916dcd3b592e5947dc625439b6e6541
praveena-pawar <praveena.dev02@gmail.com>
2026-03-26 22:18:19 +0530

    solved query 1

--- case/case_statements_practice.sql
+++ case/case_statements_practice.sql
@@ -6,3 +6,12 @@
 -- amount < 500 → 'Low'
 -- amount between 500 and 1000 → 'Medium'
 -- amount > 1000 → 'High'
+
+SELECT order_id,
+       amount,
+       CASE
+           WHEN amount < 500 THEN "Low"
+           WHEN amount BETWEEN 500 AND 1000 THEN "Medium"
+           WHEN amount > 1000 THEN "High"
+       END AS category
+FROM orders;
