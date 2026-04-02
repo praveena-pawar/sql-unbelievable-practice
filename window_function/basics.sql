60f1e98f7f44e9cf6e234c7362eda31d0d7c3c3d
praveena-pawar <praveena.dev02@gmail.com>
2026-04-02 22:52:06 +0530

    query2 : Assign a row number to each employee based on salary (highest first)

--- window_function/basics.sql
+++ window_function/basics.sql
@@ -6,3 +6,19 @@
 SELECT name, dept_id, salary,
 	MAX(salary) OVER (PARTITION BY dept_id) AS max_dept_salary
 FROM employee;
+
+
+
+
+
+
+-- Assign a row number to each employee based on salary (highest first)
+-- Output:
+-- name
+-- salary
+-- row number (1 = highest salary)
+SELECT 
+    name,
+    salary,
+    ROW_NUMBER() OVER (ORDER BY salary DESC) AS row_numbers
+FROM employee;
