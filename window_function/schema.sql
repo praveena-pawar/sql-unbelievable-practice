ab1404c29766061259e5a248c7ce96bd5c689674
praveena-pawar <praveena.dev02@gmail.com>
2026-04-02 22:49:01 +0530

    schema for window functions

---
+++ window_function/schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE employees (
+    emp_id INT,
+    name VARCHAR(50),
+    dept_id INT,
+    salary INT
+);
+
+
+
+INSERT INTO employee (emp_id, name, dept_id, salary) VALUES
+(1, 'Ravi', 101, 50000),
+(2, 'Asha', 102, 60000),
+(3, 'Kiran', 101, 70000),
+(4, 'Meena', 103, 40000),
+(5, 'John', 102, 65000);
